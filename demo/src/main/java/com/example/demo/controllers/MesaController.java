b4734ce3e1c1d58fe4e212d39d4b3c426c54dfbb
JuanLuis <juanluislopezdaw@gmail.com>
2022-11-08 15:58:17 +0100

    Create done

--- demo/src/main/java/com/example/demo/controllers/MesaController.java
+++ demo/src/main/java/com/example/demo/controllers/MesaController.java
@@ -1,23 +1,16 @@
 package com.example.demo.controllers;
 
-import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import org.apache.commons.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.models.Mesas;
@@ -27,17 +20,27 @@ import com.example.demo.respotory.MesasRespository;
 @RestController
 @RequestMapping("/api")
 public class MesaController {
-    @Autowired
+	@Autowired
 	MesasRespository mesasRespository;
 
-    @GetMapping("/mesas")
+	@GetMapping("/mesas")
 	public ResponseEntity<List<Mesas>> getAllNames() {
 		try {
 			List<Mesas> mesas = new ArrayList<Mesas>();
-            mesasRespository.findAll().forEach(mesas::add);
-            return new ResponseEntity<>(mesas, HttpStatus.OK);
+			mesasRespository.findAll().forEach(mesas::add);
+			return new ResponseEntity<>(mesas, HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@PostMapping("/mesas")
+	public ResponseEntity<Mesas> createName(@RequestBody Mesas name_mesa) {
+		try {
+			Mesas _mesaNueva = mesasRespository.save(new Mesas(name_mesa.getName()));
+			return new ResponseEntity<>(_mesaNueva, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 }
--- demo/src/main/java/com/example/demo/respotory/MesasRespository.java
+++ demo/src/main/java/com/example/demo/respotory/MesasRespository.java
@@ -1,7 +1,5 @@
 package com.example.demo.respotory;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.models.Mesas;
