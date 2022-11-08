6f30fbd0409c2d3aa3dd76c79b93c079ceca6f0c
JuanLuis <juanluislopezdaw@gmail.com>
2022-11-08 16:36:25 +0100

    CRUD end

--- demo/src/main/java/com/example/demo/controllers/MesaController.java
+++ demo/src/main/java/com/example/demo/controllers/MesaController.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +42,32 @@ public class MesaController {
 		try {
 			Mesas _mesaNueva = mesasRespository.save(new Mesas(name_mesa.getName()));
 			return new ResponseEntity<>(_mesaNueva, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@PutMapping("/mesas/{id}")
+	public ResponseEntity<Mesas> updateName(@PathVariable("id") long name_id, @RequestBody Mesas change_mesa) {
+		try {
+			java.util.Optional<Mesas> name_changed = mesasRespository.findById(name_id);
+			if (name_changed.isPresent()) {
+				Mesas _change = name_changed.get();
+				_change.setName(change_mesa.getName());
+				return new ResponseEntity<>(mesasRespository.save(_change), HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@DeleteMapping("/mesas/{id}")
+	public ResponseEntity<HttpStatus> deleteName(@PathVariable("id") long name_id) {
+		try {
+			mesasRespository.deleteById(name_id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		} catch (Exception e) {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
